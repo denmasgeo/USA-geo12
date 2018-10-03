cc0b5958d23a7c8d12e7d1a68059037911fda453
denmas <34563717+denmasgeo@users.noreply.github.com>
2018-10-03 20:26:57 +0700

    update oepoll
    new

--- line/linepy/oepoll.py
+++ line/linepy/oepoll.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from akad.ttypes import TalkException, ShouldSyncException
 from .client import LINE
 from types import *
+from threading import Thread
+import os, sys, threading, time
 
 import os, sys, threading, time
 
@@ -50,7 +53,44 @@ class OEPoll(object):
             return []
         else:
             return operations
+class MultiThreads:
+    threads = None
+    max_threads = 0.2
+    to_run = None
 
+    def __init__(self):
+        self.threads = []
+        self.to_run = []
+        try:
+            import multiprocessing
+            self.max_threads = multiprocessing.cpu_count()
+        except Exception:
+            pass
+
+    def add(self, target: callable, args: tuple):
+        self.threads.append(Thread(target=target, args=args))
+
+    def _run_processes(self, callback: callable=None, n: int = None):
+        for t in self.to_run:
+            if not n:
+                t.join()
+                callback is not None and callback()
+
+    def start(self, callback: callable=None):
+        for n, t in enumerate(self.threads):  # starting all threads
+            t.start()
+            self.to_run.append(t)
+            self._run_processes(callback, (n + 1) % self.max_threads)
+        self._run_processes(callback)
+        self.threads = []
+
+    def trace(self, threading=False):
+        try:
+            operations = self.__fetchOperation(self.client.revision)
+        except KeyboardInterrupt:
+            exit()
+        except:
+            return
     def trace(self, threading=False):
         try:
             operations = self.__fetchOperation(self.client.revision)
